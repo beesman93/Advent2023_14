5deb93aacf215cd199465127a61a87930135ad15
beesman93 <chvojtomas@gmail.com>
2023-12-14 08:05:36 +0100

    lilCleanup

--- Program.cs
+++ Program.cs
@@ -7,13 +7,13 @@ using (StreamReader reader = new(args[0]))
     }
 }
 
-const int letRocksSettleFor = 1_000;
-const int maxCycleDetect = 1_000;
-const int cycles = 1_000_000_000;
+const int letRocksSettleFor = 200;
+const int maxCycleDetect = 100;
+const int iterations = 1_000_000_000;
 
-
-solve(true);
 solve(false);
+solve(true);
+
 void solve(bool part2)
 {
     List<List<char>> map = new();
@@ -32,162 +32,87 @@ void solve(bool part2)
         int potentialCycle = -1;
         int[] cycleDetector = new int[maxCycleDetect];
 
-        for (int cycle = 1; cycle <= cycles; cycle++)
+        for (int iteration = 1; iteration <= iterations; iteration++)
         {
-            if (cycle < letRocksSettleFor)
-                _ = cycle_2(ref map);
-            else if (cycle < letRocksSettleFor + maxCycleDetect)
+            moveRocks(ref map, Cardinal.N);
+            moveRocks(ref map, Cardinal.W);
+            moveRocks(ref map, Cardinal.S);
+            moveRocks(ref map, Cardinal.E);
+            if (iteration >= letRocksSettleFor && iteration < letRocksSettleFor + maxCycleDetect)
             {
-                int curr = cycle_2(ref map);
-                cycleDetector[cycle - letRocksSettleFor] = curr;
+                int curr = totalLoad(map);
+                cycleDetector[iteration - letRocksSettleFor] = curr;
                 if (potentialCycle > 0)
                 {
-                    if (curr == cycleDetector[cycle - letRocksSettleFor - potentialCycle])
-                    {
+                    if (curr == cycleDetector[iteration - letRocksSettleFor - potentialCycle])
                         cycleConfidence++;
-                        if (cycleConfidence == potentialCycle)
-                        {
-                            //ladies and gentlemen, we got him - just skip to end
-                            while (cycle <= cycles)
-                                cycle += potentialCycle;
-                            cycle -= potentialCycle;//give room for the last couple
-                        }
-                    }
                     else
-                    {
                         potentialCycle = -1;//false positive, find another one
-                    }
                 }
-                if (cycle - letRocksSettleFor > 0)
+                if (cycleConfidence == potentialCycle)
+                {
+                    //ladies and gentlemen, we got him - just skip to end
+                    while (iteration <= iterations)
+                        iteration += potentialCycle;
+                    iteration -= potentialCycle;//give room for the last couple
+                    iteration -= potentialCycle;//give room for the last couple
+                }
+                if (iteration - letRocksSettleFor > 0)
                     if (curr == cycleDetector[0])
-                        potentialCycle = cycle - letRocksSettleFor;
+                        potentialCycle = iteration - letRocksSettleFor;
             }
-            else
+            else if(iteration > letRocksSettleFor+maxCycleDetect)
             {
-                int curr = cycle_2(ref map);
                 if (potentialCycle < 0)
                     throw new Exception("No cycle found, increase the settle rocks or cycle detect or give up");
-                else
-                {
-                    //Console.WriteLine($"{cycle}: \t\t{curr}");
-                    if(cycle==cycles)
-                        Console.WriteLine($"part2: \t\t{curr}");
-                }
+                else if (iteration == iterations)
+                    Console.WriteLine($"part2: \t\t{totalLoad(map)}");
             }
         }
     }
     else //part1
     {
-        int changeCount = -1;
-        while (changeCount != 0)
-        {
-            changeCount = 0;
-            for (int i = 1; i < map.Count; i++)
-            {
-                for (int j = 0; j < map[i].Count; j++)
-                {
-                    if (map[i][j] == 'O' && map[i - 1][j] == '.')
-                    {
-                        map[i][j] = '.';
-                        map[i - 1][j] = 'O';
-                        changeCount++;
-                    }
-                }
-            }
-
-        }
-        int curr = 0;
-        for (int i = 0; i < map.Count; i++)
-        {
-            for (int j = 0; j < map[i].Count; j++)
-            {
-                if (map[i][j] == 'O')
-                    curr += map.Count - i;
-            }
-        }
-        Console.WriteLine($"part1: \t\t{curr}");
+        moveRocks(ref map, Cardinal.N);
+        Console.WriteLine($"part1: \t\t{totalLoad(map)}");
     }
 
 }
-
-int cycle_2(ref List<List<char>> map)
+void moveRocks(ref List<List<char>> map,Cardinal dirrection)
 {
     //north, then west, then south, then east
-    int changeCount = -1;
-    while (changeCount != 0)
+    int iMove = 0;
+    int jMove = 0;
+    switch (dirrection)
     {
-        changeCount = 0;
-        for (int i = 1; i < map.Count; i++)
-        {
-            for (int j = 0; j < map[i].Count; j++)
-            {
-                if (map[i][j] == 'O' && map[i - 1][j] == '.')
-                {
-                    map[i][j] = '.';
-                    map[i - 1][j] = 'O';
-                    changeCount++;
-                }
-            }
-        }
-
+        case Cardinal.N: iMove--; break;
+        case Cardinal.S: iMove++; break;
+        case Cardinal.W: jMove--; break;
+        case Cardinal.E: jMove++; break;
     }
-
-    changeCount = -1;
-    while (changeCount != 0)
+    bool changed;
+    do
     {
-        changeCount = 0;
+        changed = false;
         for (int i = 0; i < map.Count; i++)
         {
-            for (int j = 1; j < map[i].Count; j++)
-            {
-                if (map[i][j] == 'O' && map[i][j - 1] == '.')
-                {
-                    map[i][j] = '.';
-                    map[i][j - 1] = 'O';
-                    changeCount++;
-                }
-            }
-        }
-
-    }
-
-    changeCount = -1;
-    while (changeCount != 0)
-    {
-        changeCount = 0;
-        for (int i = 0; i + 1 < map.Count; i++)
-        {
+            if (i + iMove < 0 || i + iMove >= map.Count) continue;
             for (int j = 0; j < map[i].Count; j++)
             {
-                if (map[i][j] == 'O' && map[i + 1][j] == '.')
+                if(j + jMove < 0 || j + jMove >= map[i].Count) continue;
+                if (map[i][j] == 'O' && map[i + iMove][j + jMove] == '.')
                 {
                     map[i][j] = '.';
-                    map[i + 1][j] = 'O';
-                    changeCount++;
+                    map[i + iMove][j + jMove] = 'O';
+                    changed = true;
                 }
             }
         }
 
-    }
+    } while (changed);
+}
 
-    changeCount = -1;
-    while (changeCount != 0)
-    {
-        changeCount = 0;
-        for (int i = 0; i < map.Count; i++)
-        {
-            for (int j = 0; j + 1 < map[i].Count; j++)
-            {
-                if (map[i][j] == 'O' && map[i][j + 1] == '.')
-                {
-                    map[i][j] = '.';
-                    map[i][j + 1] = 'O';
-                    changeCount++;
-                }
-            }
-        }
-
-    }
+int totalLoad(in List<List<char>> map)
+{
     int curr = 0;
     for (int i = 0; i < map.Count; i++)
     {
@@ -199,4 +124,5 @@ int cycle_2(ref List<List<char>> map)
     }
     return curr;
 }
+enum Cardinal { N, S, W, E }
 
